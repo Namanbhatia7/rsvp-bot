b0796f29e279463ee3bca68e0669b3f73bd2cbb6
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-05-24 13:07:34 +0530

    chore: deployment readiness

--- app/utils/storage.py
+++ app/utils/storage.py
@@ -1,3 +1,4 @@
+import os
 import redis
 import json
 
@@ -20,4 +21,9 @@ class RedisClient:
     def exists(self, key):
         return self.client.exists(key)
 
-redis_client = RedisClient(host='localhost', port=6379, db=0)
+redis_client = RedisClient(
+    host=os.getenv("REDIS_HOST", "localhost"), 
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    db=0,
+    decode_responses=True
+)
